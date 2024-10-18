a89ce72c0409df78edee97b4fdf710ec2579cf1d
stina dushi <stinadushi05@gmail.com>
2024-10-18 17:03:44 +0200

    added variables list

---
+++ module 1/main.py
@@ -0,0 +1,22 @@
+emri = "Stina"
+mosha = 17
+temperatura = 22.5
+print(emri)
+print(type(emri))
+print(type(mosha))
+print(type(temperatura))
+
+mosha = mosha + 10
+
+
+print(mosha)
+
+
+emri_mbiemri=emri + "Dushi"
+
+print(emri_mbiemri)
+my_data=['Stina','Dushi',17,'Shkolla Digjitale']
+print(my_data)
+my_data.append('Prishtine')
+print(my_data)
+my_data.insert(_index=
